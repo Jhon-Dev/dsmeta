931c8d117a39365fcc438c01ead0733e28e15480
jhonatan.nuss <contato@jhonatanuss.dev>
2022-07-15 21:36:30 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-
+import NotificationButton from './components/NotificationButton';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Olá mundo</h1>
-  )
+    <>
+      <h1>Olá mundo</h1>
+      <NotificationButton />
+    </>
+  );
 }
 
-export default App
+export default App;
